9e75f570d09d0f8f77dfc7b387bda9a3c411f1a6
Amir3158 <69120953+Amir3158@users.noreply.github.com>
2020-10-17 22:31:27 +0300

    Initial commit

---
+++ quiz_help.py
@@ -0,0 +1,11 @@
+import math
+import os
+import sys
+
+if __name__ == '__main__':
+    # Path
+    path = "/home"
+
+    # Join various path components
+    print(os.path.join("User/Desktop", "file.txt"))
+
